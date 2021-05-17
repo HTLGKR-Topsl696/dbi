35156327bbdd10f8d8707b2052b30f7fcd5d93bb
HTLGKR-Topsl696 <71700703+HTLGKR-Topsl696@users.noreply.github.com>
2021-05-17 14:09:00 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -5,3 +5,22 @@ SELECT last_name, hire_date FROM employees WHERE department_id = (SELECT departm
 
 ## 2
 SELECT employee_id, last_name, salary FROM employees WHERE salary > (SELECT AVG(salary) FROM employees);
+
+## 3
+SELECT employee_id, last_name FROM employees WHERE department_id IN (SELECT department_id FROM employees WHERE last_name LIKE '%u%');
+
+## 4
+### a)
+SELECT last_name, department_id, job_id FROM employees WHERE department_id IN (SELECT department_id FROM departments WHERE department_id = employees.department_id AND location_id = 1700);
+
+### b)
+SELECT last_name, department_id, job_id FROM employees WHERE department_id IN (SELECT department_id FROM departments WHERE department_id = employees.department_id AND location_id = &location_id);
+
+## 5
+SELECT last_name, salary FROM employees WHERE manager_id IN (SELECT employee_id FROM employees WHERE last_name = 'King');
+
+## 6
+SELECT department_id, last_name, job_id FROM employees WHERE department_id = (SELECT department_id FROM departments WHERE department_name = 'Executive');
+
+## 7
+SELECT employee_id, last_name, salary FROM employees WHERE salary > (SELECT AVG(salary) FROM employees) AND department_id IN (SELECT department_id FROM employees WHERE last_name LIKE '%u%');
